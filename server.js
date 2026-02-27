b95d4d46f55ee26afab01794b1e1874ef6ddc27e
erfaisal <efaisalmuzaffar@gmail.com>
2026-02-27 07:49:41 +0530

    Update server.js

--- server.js
+++ server.js
@@ -4,8 +4,7 @@ const cors = require("cors");
 
 const app = express();
 app.use(cors());
-app.use(express.json());
-
+app.use(express.json({ limit: "10mb" }));
 // Connect to MongoDB
 mongoose.connect(process.env.MONGO_URI)
   .then(() => console.log("MongoDB Connected"))
